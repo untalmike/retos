ea10b761da9eb5f74d3efd66947cc5149c76fa8d
untalmike <miguel.gonzalezl@hotmail.com>
2023-05-11 16:20:10 -0600

    Try con reto 2

--- reto_2.js
+++ reto_2.js
@@ -2,9 +2,16 @@ const year = 2022
 const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
 
 function countHours(year, holidays) {
-    const agnio = year
-    const dia = holidays[4][5].getDay()
-    console.log(dia)
+    const days = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat']
+    
+    holidays.map(e => {
+        const resultset = days[new Date(year+'/'+e).getDay()]
+        
+        if(resultset !== "Sun"){
+            
+        }
+        
+    })
 }
 
 countHours(year, holidays)
