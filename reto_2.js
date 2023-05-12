de54f86c2a18d8aa1dcd6886547555b61831979e
untalmike <miguel.gonzalezl@hotmail.com>
2023-05-12 15:25:46 -0600

    Reto 3 iniciado

--- reto_2.js
+++ reto_2.js
@@ -2,16 +2,11 @@ const year = 2022
 const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
 
 function countHours(year, holidays) {
-    const days = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat']
-    
-    holidays.map(e => {
-        const resultset = days[new Date(year+'/'+e).getDay()]
-        
-        if(resultset !== "Sun"){
-            
-        }
-        
-    })
+    const days = [1,2,3,4,5]
+    return holidays.map((holiday) => {
+        let date = new Date(`${year}/${holiday}`)
+        return days.includes(date.getDay())
+    }).reduce((count, extra) => count+extra)*2
 }
 
-countHours(year, holidays)
+console.log(countHours(year, holidays))
---
+++ reto_3.js
@@ -0,0 +1,12 @@
+const packOfGifts = ["book", "doll", "ball"]
+const reindeers = ["dasher", "dancer"]
+
+// el pack de regalos pesa 4 + 4 + 4 = 12
+// los renos pueden llevar (2 * 6) + (2 * 6) = 24
+// por lo tanto, Santa Claus puede entregar 2 cajas de regalos
+
+function distributeGifts(packOfGifts, reindeers) {
+    return packOfGifts.map(gift => {return gift.length}).reduce((count) => console.log(count))
+  }
+
+distributeGifts(packOfGifts, reindeers) // 2
